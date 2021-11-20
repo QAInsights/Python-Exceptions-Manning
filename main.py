221b743e6899401434534e0a7a317995ec6f670e
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2021-11-19 21:50:17 -0500

    Deliverable

--- main.py
+++ main.py
@@ -4,13 +4,61 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def name_exception(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    try:
+        print(name1)
+    except NameError as e:
+        print(e)
 
 
-# Press the green button in the gutter to run the script.
+def index_exception(a):
+    try:
+        for i in a:
+            print(a[4])
+    except IndexError as e:
+        print(e)
+
+
+def file_exception():
+    try:
+        with open('test', 'r') as f:
+            f.write("hello")
+    except FileNotFoundError as e:
+        print(e)
+
+
+def zero_exception():
+    try:
+        ans = 0 / 0
+        print(ans)
+    except ZeroDivisionError as e:
+        print(e)
+
+
+def syntax_exception():
+    try:
+        eval('pass = True')
+    except SyntaxError as e:
+        print(e)
+    finally:
+        print("Exit")
+
+def key_exception():
+    try:
+        ages = {'a': 30, 'b': 28, 'c': 24}
+        ages['d']
+    except KeyError as e:
+        print("KeyError")
+    finally:
+        print("Exiting")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # name_exception("Ria")
+    n = [1, 2, 3]
+    # index_exception(n)
+    file_exception()
+    zero_exception()
+    syntax_exception()
+    key_exception()
